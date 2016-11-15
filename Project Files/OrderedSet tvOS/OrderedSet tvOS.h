33d0bc79d3cfa3ebe61ef2f943048c05298a2310
Michael Zeller <mike@mikezeller.net>
2016-11-15 13:22:09 -0500

    tvOS

---
+++ Project Files/OrderedSet tvOS/OrderedSet tvOS.h
@@ -0,0 +1,19 @@
+//
+//  OrderedSet tvOS.h
+//  OrderedSet tvOS
+//
+//  Created by Michael Zeller on 11/15/16.
+//  Copyright © 2016 Weebly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OrderedSet tvOS.
+FOUNDATION_EXPORT double OrderedSet_tvOSVersionNumber;
+
+//! Project version string for OrderedSet tvOS.
+FOUNDATION_EXPORT const unsigned char OrderedSet_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OrderedSet_tvOS/PublicHeader.h>
+
+
